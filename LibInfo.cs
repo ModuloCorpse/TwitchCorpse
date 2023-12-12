e5d497f7189964b5c5c1bec55864ffd39f7bba82
ModuloCorpse <mewen.lereste@wanadoo.fr>
2023-12-12 11:25:22 +0100

    [EDIT] LibInfo update

--- LibInfo.cs
+++ LibInfo.cs
@@ -3,10 +3,7 @@
     public static class LibInfo
     {
         public static readonly Version VERSION = new("1.0.0-alpha");
-        public static readonly string[] CONTRIBUTORS = new string[]
-        {
-
-        };
+        public static readonly string[] CONTRIBUTORS = [];
         public static readonly string AUTHOR = "ModuloCorpse";
     }
 }
